ef393190219e7c664298d2992c0bc3b206549d15
Sean McGary <sean.mcgary@gmail.com>
2014-03-14 19:47:08 -0700

    update to correctly use template settings

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 var path = require('path');
-var partials = require('../index');
+var _partials = require('../index');
 var _ = require('underscore');
 
 var templatePath = path.normalize(__dirname + '/templates');
 console.log(templatePath);
-partials = new partials();
+partials = new _partials();
 
 var templates = partials.compile(templatePath);
 var serializedTemplates = partials.serializeTemplates(templates);
@@ -12,3 +12,19 @@ var serializedTemplates = partials.serializeTemplates(templates);
 
 console.log(_.keys(templates));
 console.log(serializedTemplates);
+
+var templateSettings = {
+	interpolate: /<&=([\s\S]+?)&>/g,
+	evaluate: /<&([\s\S]+?)&>/g
+};
+var lodashPartials = new _partials({
+	lodashParams: templateSettings
+});
+
+templates = lodashPartials.compile(templatePath);
+
+console.log(templates);
+
+console.log(templates['lodashTest/test']({ foobar: 'test string' }));
+
+
